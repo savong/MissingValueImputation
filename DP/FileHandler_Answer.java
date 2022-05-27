cf9f3029a744465de0ad2fabfa33ee62b19741e9
Savong Bou <38307823+savong@users.noreply.github.com>
2022-05-27 20:08:54 +0900

    Update FileHandler_Answer.java

--- DP/FileHandler_Answer.java
+++ DP/FileHandler_Answer.java
@@ -1,10 +1,13 @@
 /*
 Source code for the paper "Missing Value Imputation by Density-based Distance Likelihood" 
-(https://github.com/savong/MissingValueImputation). 
-This implementation is on top of the publicly released code (https://github.com/DLMImputation/DLM) of the method in the paper: 
+by Savong Bou (University of Tsukuba), Toshiyuki Amagasa (University of Tsukuba), 
+Hiroyuki Kitagawa (University of Tsukuba), Salman Ahmed Shaikh (AIST), and Akiyoshi Matono (AIST). 
+This implementation is on top of the publicly released code of the method in the paper: 
 Shaoxu Song and Yu Sun, "Imputing Various Incomplete Attributes via Distance likelihood," 
 the 26th ACM SIGKDD Conference on Knowledge Discovery and Data Mining, p. 535-545, 
 Virtual Event, CA, USA, August 23-27, 2020.
+
+Contact: savong-hashimoto@cs.tsukuba.ac.jp
 */
 
 /*
